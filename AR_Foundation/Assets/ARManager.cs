dc35afedcc99856527de456b60de9663bfe2fbad
arielxioxio <71679945+arielxioxio@users.noreply.github.com>
2020-11-24 12:00:38 +0800

    點點

---
+++ AR_Foundation/Assets/ARManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(ARRaycastManager))]
+
+public class ARManager : MonoBehaviour
+{
+    [Header("點擊後產生物件")]
+    public GameObject obj;
+    [Header("AR管理器")]
+    public ARRaycastManager arManager;
+
+    private Vector2 pointMouse;
+    private List<ARRaycastHit> hits;
+
+    private void Tap()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            pointMouse = Input.mousePosition;
+            print(pointMouse);
+        }
+    }
+    private void Update()
+    {
+        Tap();
+    }
+}
